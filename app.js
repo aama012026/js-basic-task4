ce962f17ad31e8171cfd557d295e8996b13f2734
aama012026 <aama012026@gmail.com>
2026-03-20 23:45:26 +0100

    Added drag to select color on user color wheel, but broke light values for derivative color dots.

--- app.js
+++ app.js
@@ -10,14 +10,44 @@ let lightnessStops = 3;
 let usercolor = {
 	h: getRandomValues(0, 360)[0],
 	s: getRandomValues(0, 1)[0],
-	l: getRandomValues(-1, 1)[0]
+	l: 0
 };
+
+let isDragging = false;
+
+createColorDot(originWheel, usercolor);
+const userColorDot = originWheel.querySelector('.circle');
 
 const colors = {
 	hues: [],
 	saturationLevels: [],
 	lightnessPoints: []
 };
+
+originWheel.addEventListener('mousedown', (e) => {
+	isDragging = true;
+	updateUserDot(e);	
+});
+
+originWheel.addEventListener('mousemove', (e) => {
+ 	if (isDragging) {
+		updateUserDot(e);
+	}
+});
+
+document.addEventListener('mouseup', () => isDragging = false );
+
+function updateUserDot(e) {
+	const boundingBox = originWheel.getBoundingClientRect();
+	const cartesianCoord = {
+		x: getBipolarFromNormalizedPositive((e.clientX - boundingBox.left) / boundingBox.width),
+		y: getBipolarFromNormalizedPositive(1 - ((e.clientY - boundingBox.top) / boundingBox.height))
+	};
+	const polarCoord = polarFromCartesian(cartesianCoord.x, cartesianCoord.y);
+	usercolor.h = polarCoord.angle;
+	usercolor.s = polarCoord.radius;
+	moveColorDot(userColorDot, usercolor);
+}
 
 function generateColors() {
 	colors.hues = getHueShifts(usercolor.h, hueStops);
@@ -73,7 +103,7 @@ function createColorDot(parent, color) {
 	const dot = document.createElement('span');
 	dot.classList.add('circle');
 	dot.style.width = `${dotRadius}rem`;
-	dot.style.background = `hsl(${color.h} ${color.s*100} ${color.l})`;
+	dot.style.background = `hsl(${color.h} ${color.s*100} ${getPositiveFromNormalizedBipolar(color.l)*100})`;
 	dot.style.position = 'absolute';
 	dot.style.left = `calc(50% + ${position.x} * 50% - ${dotRadius}rem/2)`;
 	dot.style.bottom = `calc(50% + ${position.y} * 50% - ${dotRadius}rem/2)`;
@@ -83,19 +113,31 @@ function createColorDot(parent, color) {
 	parent.appendChild(dot);
 }
 
+function moveColorDot(element, color) {
+	const position = cartesianFromPolar(color.s, color.h);
+	element.style.background = `hsl(${color.h} ${color.s * 100} ${getPositiveFromNormalizedBipolar(color.l) * 100})`;
+	element.style.left = `calc(50% + ${position.x} * 50% - ${dotRadius}rem/2)`;
+	element.style.bottom = `calc(50% + ${position.y} * 50% - ${dotRadius}rem/2)`;
+}
+
 function createToneWheel(container, lightness) {
 	const toneWheel = document.createElement('span');
 	const lightnessCssValue = getPositiveFromNormalizedBipolar(lightness) * 100;
 	toneWheel.style.setProperty('--lightness', `${lightnessCssValue}`);
 	toneWheel.classList.add('circle', 'tone-wheel');
 	toneWheel.style.width = `calc(var(--circle-radius) * (1 - ${Math.abs(lightness)})`;
-	toneWheel.dataset.lightness = lightnessCssValue;
+	toneWheel.dataset.lightness = lightness;
 	container.appendChild(toneWheel);
 }
 
 function cartesianFromPolar(r, a) {
 	const radianAngle = a * (Math.PI / 180);
 	return { x: r * Math.cos(radianAngle), y: r * Math.sin(radianAngle) }
+}
+function polarFromCartesian(x, y) {
+	const r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	const a = Math.atan2(y, x) * (180 / Math.PI);
+	return {radius: r, angle: a < 0 ? a + 360 : a};
 }
 
 function getRest(n) { return n < 1 ? n : n % Math.floor(n) }
